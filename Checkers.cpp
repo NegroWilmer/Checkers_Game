c690068b6754ad360f655cd8dab2585e001c0261
NegroWilmer <wilpala@outlook.com>
2025-11-02 19:41:03 -0500

    Update Checkers.cpp

--- Checkers.cpp
+++ Checkers.cpp
@@ -5,82 +5,96 @@
 
 const int SIZE = 10;
 
-using std::cout;
+using namespace std;
 
 char board[SIZE][SIZE];
 
 void initBoard() {
-    for (int i = 0; i < SIZE; ++i) {
-        for (int j = 0; j < SIZE; ++j) {
+	for (int i = 0; i < SIZE; ++i) {
+		for (int j = 0; j < SIZE; ++j) {
 			board[i][j] = ((i + j) % 2 == 1) ? '.' : ' ';
-        }
-    }
-    
-    for(int i=0; i < 4; ++i) {
-        for(int j = 0; j < SIZE; ++j) {
-            if((i + j) % 2 == 1) {
-                board[i][j] = 'b'; // Player 1 pieces
-            }
-        }
+		}
 	}
 
-    for(int i=6; i < SIZE; ++i) {
-        for(int j = 0; j < SIZE; ++j) {
-            if((i + j) % 2 == 1) {
-                board[i][j] = 'w'; // Player 2 pieces
-            }
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < SIZE; ++j) {
+			if ((i + j) % 2 == 1) {
+				board[i][j] = 'b'; 
+			}
+		}
+	}
+
+	for (int i = 6; i < SIZE; ++i) {
+		for (int j = 0; j < SIZE; ++j) {
+			if ((i + j) % 2 == 1) {
+				board[i][j] = 'w'; 
+			}
 		}
 
 
-}
+	}
 }
 
 void printBoard() {
-    cout << " ";
-    for(int i=0;i < SIZE;++i)
+	cout << " ";
+	for (int i = 0; i < SIZE; ++i)
 		cout << i << " ";
 	cout << "\n";
 
 
 
 
-    for (int i = 0; i < SIZE; ++i) {
-        cout << i << " ";
-        for (int j = 0; j < SIZE; ++j) {
-            cout << board[i][j] << " ";
-        }
-        cout << "\n";
-    }
+	for (int i = 0; i < SIZE; ++i) {
+		cout << i << " ";
+		for (int j = 0; j < SIZE; ++j) {
+			cout << board[i][j] << " ";
+		}
+		cout << "\n";
+	}
 }
 
 bool movePiece(int fromRow, int fromCol, int toRow, int toCol, char player) {
-    if (toRow < 0 || toRow >= SIZE || toCol < 0 || toCol >= SIZE)
-        return false;
-    if (board[toRow][toCol] != player || board[toRow][toCol] != '.')
-        return false;
+	
+	if (fromRow < 0 || fromRow >= SIZE || fromCol < 0 || fromCol >= SIZE)
+		return false;
+	if (toRow < 0 || toRow >= SIZE || toCol < 0 || toCol >= SIZE)
+		return false;
 
+	
+	if (board[fromRow][fromCol] != player)
+		return false;
 
-    int dir = (player == 'w') ? -1 : 1;
-    int rowDiff = toRow - fromRow;
-    int colDiff = toCol - fromCol;
+	
+	if (board[toRow][toCol] != '.')
+		return false;
 
-    if (rowDiff == dir && abs(colDiff) == 1)
-        {
-        board[toRow][toCol] = player;
-        board[fromRow][fromCol] = '.';
-        return true;
+	int dir = (player == 'w') ? -1 : 1;
+	int rowDiff = toRow - fromRow;
+	int colDiff = toCol - fromCol;
+
+	
+	if (rowDiff == dir && abs(colDiff) == 1)
+	{
+		board[toRow][toCol] = player;
+		board[fromRow][fromCol] = '.';
+		return true;
 	}
 
-    if(rowDiff == 2 * dir && abs(colDiff) == 2) {
-        int midRow = fromRow + dir;
-        int midCol = fromCol + (colDiff / 2);
-        char opponent = (player == 'w') ? 'b' : 'w';
-   
-        if(board[midRow][midCol] == opponent) {
-            board[toRow][toCol] = player;
-            board[fromRow][fromCol] = '.';
-            board[midRow][midCol] = '.';
-            return true;
+	
+	if (rowDiff == 2 * dir && abs(colDiff) == 2) {
+		int midRow = fromRow + dir;
+		int midCol = fromCol + (colDiff / 2);
+
+		if (midRow < 0 || midRow >= SIZE || midCol < 0 || midCol >= SIZE)
+			return false;
+
+		char opponent = (player == 'w') ? 'b' : 'w';
+
+		if (board[midRow][midCol] == opponent) {
+			board[toRow][toCol] = player;
+			board[fromRow][fromCol] = '.';
+			board[midRow][midCol] = '.';
+			return true;
 		}
 	}
 
@@ -93,10 +107,11 @@ bool isGameOver(char& winner) {
 		for (int j = 0; j < SIZE; ++j) {
 			if (board[i][j] == 'w') rCout++;
 			if (board[i][j] == 'b') bCout++;
-	}
+		}
 
-	if (rCout == o) {
-		winner = 'b'; return true
+	if (rCout == 0) {
+		winner = 'b';
+		return true;
 	}
 	if (bCout == 0) {
 		winner = 'w'; return true;
@@ -113,27 +128,27 @@ int main() {
 
 	while (true) {
 		printBoard();
-		cout << "jugador" << player << "'s turn (format: fromRow fromCol toRow toCol): ";
+		cout << "jugador " << player << "'s turn (format: fromRow fromCol toRow toCol): ";
 		int fromRow, fromCol, toRow, toCol;
-		cin >> fronRow >> fromCol >> toRow >> toCol;
+		cin >> fromRow >> fromCol >> toRow >> toCol;
 
 		if (!cin) {
 			cout << "entrada no valida. Saliendo. \n";
-			getch();
+			(void)_getch();
 			break;
 		}
 		if (movePiece(fromRow, fromCol, toRow, toCol, player)) {
 			if (isGameOver(winner)) {
 				printBoard();
 				cout << "Game over! jugador " << player << "wins\n";
-				getch();
+				(void)_getch();
 				break;
 			}
 			player = (player == 'w') ? 'b' : 'w';
 		}
 		else {
 			cout << "movimiento invalido. intente de nuevo \n";
-			getch();
+			(void)_getch();
 		}
 	}
 	return 0;
