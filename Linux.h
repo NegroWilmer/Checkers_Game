736ff2de3e2ff664b8bdb2be6b492832ce8dffdd
mariocbr1234-sys <mariocbr1234@gmail.com>
2025-11-04 10:34:52 -0500

    Add files via upload

---
+++ Linux.h
@@ -0,0 +1,22 @@
+#ifndef LINUX_H
+#define LINUX_H
+
+#ifdef _WIN32
+#include <conio.h>
+#else
+#include <termios.h>
+#include <unistd.h>
+
+inline int _getch() {
+    struct termios oldt, newt;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    int ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return ch;
+}
+#endif
+
+#endif 
