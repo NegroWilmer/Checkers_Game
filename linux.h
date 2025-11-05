512cbbabf375ea31472c36381d501fb5c3aa04f8
mariocbr1234-sys <mariocbr1234@gmail.com>
2025-11-05 10:08:41 -0500

    Refactor _getch function declaration in linux.h

--- linux.h
+++ linux.h
@@ -4,20 +4,25 @@
 #ifdef _WIN32
 #include <conio.h>
 #else
+int _getch();
+
 #include <termios.h>
 #include <unistd.h>
 
-inline int _getch() {
+int _getch() {
     struct termios oldt, newt;
     tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
     newt.c_lflag &= ~(ICANON | ECHO);
     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
     int ch = getchar();
+
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
     return ch;
 }
 #endif
 
 #endif 
+
 
