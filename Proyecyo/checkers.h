c2c27c9df2ef742b2d8cd78ab0e8d6f8ad6029db
NegroWilmer <wilpala@outlook.com>
2025-11-12 01:26:41 -0500

    Expand checkers.h with additional game functions
    Added new functions for board initialization, size retrieval, and player management.

--- Proyecyo/checkers.h
+++ Proyecyo/checkers.h
@@ -1,6 +1,16 @@
 #ifndef CHECKERS_H
 #define CHECKERS_H
 
-void jugarDamas(); 
+
+void jugarDamas();
+
+
+void initBoard();                 
+int getBoardSize();           
+char getCell(int r, int c);       
+char getCurrentPlayer();           
+
+bool tryMoveGUI(int fromRow, int fromCol, int toRow, int toCol, char &winner);
 
 #endif
+
